223dfdb6ad72fd9c107ef543fe4d8a1a3bd909c8
ifxscript <ianekwe7@gmail.com>
2026-07-30 21:50:06 +0100

    Reduce SquareArcRow stroke weight to width * 0.012 for cleaner, sleeker outlines

--- public/motor/motor.js
+++ public/motor/motor.js
@@ -2835,7 +2835,7 @@ class SquareArcRow {
             return;
         }
 
-        strokeWeight(width * 0.0175);
+        strokeWeight(width * 0.012);
 
         for (let i = -1; i < count; i++) {
             let a = this.startA + i * step + phase;
